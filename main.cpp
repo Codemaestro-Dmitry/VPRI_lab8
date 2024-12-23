64e3760c1f999fa2f252bbbc8af110480e66e912
Dmitry <CodeMaestroDmitry@yandex.ru>
2024-12-24 01:42:44 +0300

    Добавление программы "сумма чисел"

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"sdt.h"
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "inpute first digit: ";
+	cin >> a;
+	cout << "inpute second digit: ";
+	cin >> b;
+	cout << "Result: " << a + b;
+}
